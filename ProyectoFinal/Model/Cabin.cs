ef63033009cba7f5a9edd60e31d34dbaaafb1458
[nivekino] <[00100719@uca.edu.sv]>
2021-06-25 02:19:09 -0600

    adding first migration and adding login functions

---
+++ ProyectoFinal/Model/Cabin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProyectoFinal
+{
+    public partial class Cabin
+    {
+        public Cabin()
+        {
+            Citizens = new HashSet<Citizen>();
+            Employeexcabins = new HashSet<Employeexcabin>();
+        }
+
+        public int Id { get; set; }
+        public string Direction { get; set; }
+        public string Phone { get; set; }
+        public string ManagerName { get; set; }
+        public string Mail { get; set; }
+
+        public virtual ICollection<Citizen> Citizens { get; set; }
+        public virtual ICollection<Employeexcabin> Employeexcabins { get; set; }
+    }
+}
---
+++ ProyectoFinal/Model/Employeexcabin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProyectoFinal
+{
+    public partial class Employeexcabin
+    {
+        public int Id { get; set; }
+        public int? CabinId { get; set; }
+        public int? EmployeeId { get; set; }
+        public DateTime? Login { get; set; }
+        public DateTime? Logout { get; set; }
+
+        public virtual Cabin Cabin { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
