69f57dd3e133a68a8f01ef7d28d27e6d1c8c5c3d
[nivekino] <[00100719@uca.edu.sv]>
2021-06-25 22:41:23 -0600

    First migration

---
+++ ProyectoFinal/Migrations/20210626043822_init.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace ProyectoFinal.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cabin",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    direction = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    phone = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: true),
+                    manager_name = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    mail = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cabin", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "employee",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    employee_name = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    institutional_mail = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    direction = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    employee_type = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: true),
+                    employee_user = table.Column<string>(type: "varchar(25)", maxLength: 25, nullable: true),
+                    employee_password = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employee", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "citizen",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    DUI = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: true),
+                    citizen_name = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    address = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    phone = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: true),
+                    age = table.Column<int>(type: "int", nullable: true),
+                    institutional_id = table.Column<int>(type: "int", nullable: true),
+                    mail = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: true),
+                    cabin_id = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_citizen", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_cabin",
+                        column: x => x.cabin_id,
+                        principalTable: "cabin",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "employeexcabin",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    cabin_id = table.Column<int>(type: "int", nullable: true),
+                    employee_id = table.Column<int>(type: "int", nullable: true),
+                    login = table.Column<DateTime>(type: "datetime", nullable: true),
+                    logout = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employeexcabin", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_employexcabin",
+                        column: x => x.cabin_id,
+                        principalTable: "cabin",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_employexcabin_employee",
+                        column: x => x.employee_id,
+                        principalTable: "employee",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "appointment_one",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    date_appointment = table.Column<DateTime>(type: "datetime", nullable: true),
+                    place = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    row_date = table.Column<DateTime>(type: "datetime", nullable: true),
+                    vaccination_date = table.Column<DateTime>(type: "datetime", nullable: true),
+                    side_effect_time = table.Column<TimeSpan>(type: "time", nullable: true),
+                    employee_id = table.Column<int>(type: "int", nullable: true),
+                    citizen_id = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_appointment_one", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_citizen_one",
+                        column: x => x.citizen_id,
+                        principalTable: "citizen",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_employee_one",
+                        column: x => x.employee_id,
+                        principalTable: "employee",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "appointment_twoo",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    date_appointment = table.Column<DateTime>(type: "datetime", nullable: true),
+                    place = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    row_date = table.Column<DateTime>(type: "datetime", nullable: true),
+                    vaccination_date = table.Column<DateTime>(type: "datetime", nullable: true),
+                    side_effect_time = table.Column<TimeSpan>(type: "time", nullable: true),
+                    employee_id = table.Column<int>(type: "int", nullable: true),
+                    citizen_id = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_appointment_twoo", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_citizen_twoo",
+                        column: x => x.citizen_id,
+                        principalTable: "citizen",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_employee_twoo",
+                        column: x => x.employee_id,
+                        principalTable: "employee",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "disease",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    diseasea_name = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true),
+                    citizen_id = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_disease", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_citizen",
+                        column: x => x.citizen_id,
+                        principalTable: "citizen",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "institutional_id",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    identificator = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: true),
+                    citizen_id = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_institutional_id", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_inst_id_citizen",
+                        column: x => x.citizen_id,
+                        principalTable: "citizen",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "FK_citizen_one",
+                table: "appointment_one",
+                column: "citizen_id");
+
+            migrationBuilder.CreateIndex(
+                name: "FK_employee_one",
+                table: "appointment_one",
+                column: "employee_id");
+
+            migrationBuilder.CreateIndex(
+                name: "FK_citizen_twoo",
+                table: "appointment_twoo",
+                column: "citizen_id");
+
+            migrationBuilder.CreateIndex(
+                name: "FK_employee_twoo",
+                table: "appointment_twoo",
+                column: "employee_id");
+
+            migrationBuilder.CreateIndex(
+                name: "FK_cabin",
+                table: "citizen",
+                column: "cabin_id");
+
+            migrationBuilder.CreateIndex(
+                name: "FK_citizen",
+                table: "disease",
+                column: "citizen_id");
+
+            migrationBuilder.CreateIndex(
+                name: "FK_employexcabin",
+                table: "employeexcabin",
+                column: "cabin_id");
+
+            migrationBuilder.CreateIndex(
+                name: "FK_employexcabin_employee",
+                table: "employeexcabin",
+                column: "employee_id");
+
+            migrationBuilder.CreateIndex(
+                name: "FK_inst_id_citizen",
+                table: "institutional_id",
+                column: "citizen_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "appointment_one");
+
+            migrationBuilder.DropTable(
+                name: "appointment_twoo");
+
+            migrationBuilder.DropTable(
+                name: "disease");
+
+            migrationBuilder.DropTable(
+                name: "employeexcabin");
+
+            migrationBuilder.DropTable(
+                name: "institutional_id");
+
+            migrationBuilder.DropTable(
+                name: "employee");
+
+            migrationBuilder.DropTable(
+                name: "citizen");
+
+            migrationBuilder.DropTable(
+                name: "cabin");
+        }
+    }
+}
---
+++ ProyectoFinal/Model/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProyectoFinal
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            AppointmentOnes = new HashSet<AppointmentOne>();
+            AppointmentTwoos = new HashSet<AppointmentTwoo>();
+            Employeexcabins = new HashSet<Employeexcabin>();
+        }
+
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public string InstitutionalMail { get; set; }
+        public string Direction { get; set; }
+        public string EmployeeType { get; set; }
+        public string EmployeeUser { get; set; }
+        public string EmployeePassword { get; set; }
+
+        public virtual ICollection<AppointmentOne> AppointmentOnes { get; set; }
+        public virtual ICollection<AppointmentTwoo> AppointmentTwoos { get; set; }
+        public virtual ICollection<Employeexcabin> Employeexcabins { get; set; }
+    }
+}
