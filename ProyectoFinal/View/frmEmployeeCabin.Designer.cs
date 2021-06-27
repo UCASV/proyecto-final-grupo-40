b92474e0c73f1656d4b8de12ca520d4028fb4128
Nelson00305418 <00305418@uca.edu.sv>
2021-06-26 19:39:26 -0600

    fixed the cabin form

--- ProyectoFinal/View/frmEmployeeCabin.Designer.cs
+++ ProyectoFinal/View/frmEmployeeCabin.Designer.cs
@@ -36,7 +36,6 @@ namespace ProyectoFinal.View
             this.lblTextLogin2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -44,7 +43,6 @@ namespace ProyectoFinal.View
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -77,7 +75,7 @@ namespace ProyectoFinal.View
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(78, 131);
+            this.label1.Location = new System.Drawing.Point(78, 171);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(215, 19);
             this.label1.TabIndex = 4;
@@ -89,24 +87,12 @@ namespace ProyectoFinal.View
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(128, 179);
+            this.label2.Location = new System.Drawing.Point(6, 214);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(165, 19);
+            this.label2.Size = new System.Drawing.Size(287, 19);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Seguimiento de citas:";
+            this.label2.Text = "Paso 1.Seguimiento de citas/Prechequeo:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // label3
-            // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(135, 223);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(158, 19);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Paso 1. Prechequeo:";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label4
             // 
@@ -204,21 +190,6 @@ namespace ProyectoFinal.View
             this.button3.Text = "PASO 1";
             this.button3.UseVisualStyleBackColor = false;
             // 
-            // button4
-            // 
-            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (43)))), ((int) (((byte) (48)))), ((int) (((byte) (150)))));
-            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int) (((byte) (43)))), ((int) (((byte) (48)))), ((int) (((byte) (150)))));
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(336, 170);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(130, 28);
-            this.button4.TabIndex = 24;
-            this.button4.Text = "SEGUIMIENTO";
-            this.button4.UseVisualStyleBackColor = false;
-            // 
             // button5
             // 
             this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
@@ -227,7 +198,7 @@ namespace ProyectoFinal.View
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.button5.ForeColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(336, 122);
+            this.button5.Location = new System.Drawing.Point(336, 162);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(130, 28);
             this.button5.TabIndex = 25;
@@ -241,7 +212,6 @@ namespace ProyectoFinal.View
             this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (31)))), ((int) (((byte) (65)))), ((int) (((byte) (185)))));
             this.ClientSize = new System.Drawing.Size(517, 529);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -249,7 +219,6 @@ namespace ProyectoFinal.View
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblTextLogin2);
@@ -265,8 +234,6 @@ namespace ProyectoFinal.View
 
         private System.Windows.Forms.Button button5;
 
-        private System.Windows.Forms.Button button4;
-
         private System.Windows.Forms.Button button3;
 
         private System.Windows.Forms.Button button2;
@@ -279,8 +246,6 @@ namespace ProyectoFinal.View
         private System.Windows.Forms.Label label5;
 
         private System.Windows.Forms.Label label4;
-
-        private System.Windows.Forms.Label label3;
 
         private System.Windows.Forms.Label label2;
 
